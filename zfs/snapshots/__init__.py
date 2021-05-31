7c06fd2d6e1420bccea786140e10093b620dc5e8
Anton Hvornum <anton@hvornum.se>
2021-05-31 22:41:59 +0200

    Added contexts

--- zfs/snapshots/__init__.py
+++ zfs/snapshots/__init__.py
@@ -4,8 +4,18 @@ class Delta:
 	def __init__(self, origin, destination):
 		self.origin = origin
 		self.destination = destination
+		self.worker = None
+
+	def __enter__(self):
 		self.worker = Popen(["zfs", "send", "-c", "-I", origin, destination], shell=False, stdout=PIPE, stderr=PIPE)
-	
+
+	def __exit__(self, *args):
+		print(args)
+		if self.worker:
+			self.worker.stdout.close()
+			self.worker.stdin.close()
+			self.worker.stderr.close()
+
 	def is_alive(self):
 		return self.worker.poll() is None
 
@@ -16,6 +26,17 @@ class Delta:
 class DeltaReader:
 	def __init__(self):
 		self.worker = None
+		self.transfer_id = None
+
+	def __enter__(self):
+		self.worker = Popen(["zfs", "recv", frame['information']['destination_name']], shell=False, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+
+	def __exit__(self, *args):
+		print(args)
+		if self.worker:
+			self.worker.stdout.close()
+			self.worker.stdin.close()
+			self.worker.stderr.close()
 
 	def restore(self, frame):
-		print('Restoring frame:', frame)
+		self.worker.write(frame['data'])
