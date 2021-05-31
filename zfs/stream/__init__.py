201da0a26deab001e5c7643cd485dcec6238ac3c
Anton Hvornum <anton@hvornum.se>
2021-05-31 08:16:14 +0200

    Update __init__.py

--- zfs/stream/__init__.py
+++ zfs/stream/__init__.py
@@ -4,13 +4,14 @@ import socket
 import struct
 import zlib
 
-def structure_data(transfer_id, data, index):
+def structure_data(transfer_id, index, previous_data=None, data=None):
 	return (
 			struct.pack('B', transfer_id)
 			+struct.pack('B', index)
-			+struct.pack('I', zlib.crc32(data) & 0xffffffff)
+			+struct.pack('I', zlib.crc32(data) & 0xffffffff if data else zlib.crc32(previous_data) & 0xffffffff)
 			+struct.pack('H', len(data))
-			+data
+			+(data if data else b'')
+			+(previous_data if previous_data else b'')
 	)
 
 def deliver(stream, to, on_send=None):
@@ -21,12 +22,14 @@ def deliver(stream, to, on_send=None):
 	sender = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 	transfer_id = 1
 	frame_index = 0
+	previous_data = None
 	while (data := stream.read(1024)):
-		frame = structure_data(transfer_id, data, frame_index)
-		
+		frame = structure_data(transfer_id, frame_index, previous_data, data)
+
 		if on_send:
 			frame = on_send(frame)
 
 		sender.sendto(frame, to)
-		sender.sendto(frame, to)
+		
+		previous_data = data
 		frame_index += 1
