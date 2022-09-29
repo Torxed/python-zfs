c070e4c93a789d8e271e63f16499d2bc4baeb631
Anton Hvornum <anton.feeds@gmail.com>
2022-09-29 14:07:26 +0000

    Adding worker

--- zfs/workers.py
+++ zfs/workers.py
@@ -1,6 +1,7 @@
 import logging
 from .snapshots.delta import DeltaReader
 from .pool import PoolRestore
+from .dataset import DatasetRestore
 from .logger import log
 
 workers = {}
@@ -15,3 +16,6 @@ def setup_worker(transfer_id, information):
 	elif information[0] == 1:
 		log(f"Setting up a PoolRestore reader for {information}", level=logging.INFO, fg="grey")
 		workers[transfer_id] = PoolRestore(information)
+	elif information[0] == 0:
+		log(f"Setting up a DatasetRestore reader for {information}", level=logging.INFO, fg="grey")
+		workers[transfer_id] = DatasetRestore(information)
